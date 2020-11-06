57e926764df085e708cef27facef4fc1672ffae6
Stefan Bauer <stefan.bauer08@web.de>
2020-11-06 14:19:51 +0100

    optimized imports

--- src/main/java/de/othr/sw/bank/repo/CustomerRepository.java
+++ src/main/java/de/othr/sw/bank/repo/CustomerRepository.java
@@ -3,7 +3,5 @@ package de.othr.sw.bank.repo;
 import de.othr.sw.bank.entity.Customer;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface CustomerRepository extends CrudRepository<Customer,Long> {
 }
