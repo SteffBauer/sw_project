d8d5d9ae856fff34b5ffb0070ba2c1a67c60f255
Stefan Bauer <stefan.bauer08@web.de>
2020-11-06 14:25:03 +0100

    worked on Transfer

--- src/main/java/de/othr/sw/bank/entity/Transfer.java
+++ src/main/java/de/othr/sw/bank/entity/Transfer.java
@@ -13,10 +13,79 @@ public class Transfer {
     private String description;
 
     @ManyToOne
-    @JoinColumn(name="payer_account_id")
+    @JoinColumn(name = "payer_account_id")
     private Account payerAccount;
 
     @ManyToOne
-    @JoinColumn(name="receiver_account_id")
+    @JoinColumn(name = "receiver_account_id")
     private Account receiverAccount;
+
+    public Transfer() {
+    }
+
+    public Transfer(double amount, String description, Account payerAccount, Account receiverAccount) {
+        this.amount = amount;
+        this.description = description;
+        this.payerAccount = payerAccount;
+        this.receiverAccount = receiverAccount;
+        this.date = new Date();
+    }
+
+    public long getTransferId() {
+        return transferId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Account getPayerAccount() {
+        return payerAccount;
+    }
+
+    public void setPayerAccount(Account payerAccount) {
+        this.payerAccount = payerAccount;
+    }
+
+    public Account getReceiverAccount() {
+        return receiverAccount;
+    }
+
+    public void setReceiverAccount(Account receiverAccount) {
+        this.receiverAccount = receiverAccount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!o.getClass().equals(Transfer.class))
+            return false;
+        Transfer other = (Transfer) o;
+
+        return this.hashCode() == other.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(transferId);
+    }
 }
