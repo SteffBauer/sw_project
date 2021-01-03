68684657d486afc61c3aa1d70af9b47fec6b7750
Bauer Stefan <stefan.bauer08@web.de>
2021-01-03 18:25:44 +0100

    resolve todo move salt to application.properties

--- src/main/java/de/othr/sw/bank/utils/EncryptionUtils.java
+++ src/main/java/de/othr/sw/bank/utils/EncryptionUtils.java
@@ -1,5 +1,6 @@
 package de.othr.sw.bank.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,11 +9,14 @@ import java.security.SecureRandom;
 
 @Configuration
 public class EncryptionUtils {
-    // todo @Value("${application-config.user-password-salt}")
-    private static final String salt = "Super-streng-geh€1m";
+    private static final int STRENGTH = 15;
+
+    @Value("${user-password-salt}")
+    private  String salt;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(15, new SecureRandom(salt.getBytes()));
+        return new BCryptPasswordEncoder(STRENGTH, new SecureRandom(salt.getBytes()));
     }
 }
 
