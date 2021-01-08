2b9dbff147444fa8a46c1a892c3609b1c88182e0
Bauer Stefan <stefan.bauer08@web.de>
2021-01-08 18:20:15 +0100

    work on chat views for customer and employee

--- src/main/java/de/othr/sw/bank/controller/SupportController.java
+++ src/main/java/de/othr/sw/bank/controller/SupportController.java
@@ -5,6 +5,7 @@ import de.othr.sw.bank.entity.Customer;
 import de.othr.sw.bank.entity.Employee;
 import de.othr.sw.bank.entity.Message;
 import de.othr.sw.bank.entity.Person;
+import de.othr.sw.bank.service.EmployeeServiceIF;
 import de.othr.sw.bank.service.SupportServiceIF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/support")
@@ -22,23 +24,31 @@ public class SupportController {
 
     @Autowired
     private SupportServiceIF supportService;
+    @Autowired
+    private EmployeeServiceIF employeeService;
 
     @GetMapping
-    public String getSupportChat(Model model) {
-        // todo for testing website
-        //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        //long id = ((Person) authentication.getPrincipal()).getId();
+    public String getSupportChatView(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        long id = ((Person) authentication.getPrincipal()).getId();
         List<Message> messages = supportService.pullMessages("ServiceToken");
         model.addAttribute("messages", messages);
-/*
+
         if (authentication.getPrincipal() instanceof Employee) {
             model.addAttribute("isEmployee", true);
+
+            Optional<Employee> employee = employeeService.getEmployeeByUsername(((Employee) authentication.getPrincipal()).getUsername());
+
+            if(employee != null)
+                model.addAttribute("customers", employee.get().getActiveCustomers());
+
             return "/employee/supportChat.html";
         }
 
- */
+
         model.addAttribute("isEmployee", false);
-        return "/customer/supportChat.html";
+        //return "/customer/supportChat.html";
+        return "/employee/supportChat.html";
 
 
     }
