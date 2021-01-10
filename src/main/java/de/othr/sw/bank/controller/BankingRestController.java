a510754f0a34be442fd8d269f640d78cbded6302
Bauer Stefan <stefan.bauer08@web.de>
2021-01-10 15:54:41 +0100

    implement api paths for other projects

---
+++ src/main/java/de/othr/sw/bank/controller/BankingRestController.java
@@ -0,0 +1,48 @@
+package de.othr.sw.bank.controller;
+
+import de.othr.sw.bank.entity.AccountRequest;
+import de.othr.sw.bank.entity.TransferRequest;
+import de.othr.sw.bank.service.AccountNotFoundException;
+import de.othr.sw.bank.service.BankingServiceIF;
+import de.othr.sw.bank.service.InvalidTransferException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/banking")
+public class BankingRestController {
+
+    @Autowired
+    private BankingServiceIF bankingServiceIF;
+
+    @RequestMapping("/accounts")
+    public ResponseEntity<Long> getAccountValue(AccountRequest accountRequest) {
+        return bankingServiceIF.getAccountValue(accountRequest);
+    }
+
+    @PostMapping("/accounts/transfers/transfer")
+    public ResponseEntity<TransferRequest> transferMoney(TransferRequest transferRequest) {
+        try {
+            return bankingServiceIF.transferMoney(transferRequest);
+        } catch (AccountNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (InvalidTransferException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/accounts/transfers/mandate")
+    public ResponseEntity<TransferRequest> mandateMoney(TransferRequest transferRequest) {
+        try {
+            return bankingServiceIF.mandateMoney(transferRequest);
+        } catch (AccountNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (InvalidTransferException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+}
---
+++ src/main/java/de/othr/sw/bank/controller/CustomerRestController.java
@@ -0,0 +1,21 @@
+package de.othr.sw.bank.controller;
+
+import de.othr.sw.bank.entity.Customer;
+import de.othr.sw.bank.service.CustomerServiceIF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerServiceIF customerService;
+
+    @RequestMapping
+    public ResponseEntity<Customer> getAccountValue(String taxNumber) {
+        return customerService.findCustomer(taxNumber);
+    }
+}
