ec4aa932c7bb976ffd9b6a95586ae369b3e6804b
Stefan Bauer <stefan.bauer08@web.de>
2021-01-24 15:55:49 +0100

    add transaction annotation

--- src/main/java/de/othr/sw/bank/service/EmployeeService.java
+++ src/main/java/de/othr/sw/bank/service/EmployeeService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +47,7 @@ public class EmployeeService implements EmployeeServiceIF, UserDetailsService {
     }
 
     @Override
+    @Transactional
     public void removeCustomerFromEmployee(Customer customer) {
         Optional<Employee> optionalEmployee = employeeRepository.findById(customer.getAttendant().getId());
         optionalEmployee.ifPresent(e -> {
